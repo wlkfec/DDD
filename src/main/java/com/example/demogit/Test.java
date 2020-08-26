7a98e7cc7decf926969877a4497935f5990812a4
wangjiankai <wangjiankai@unisound.com>
2020-08-26 22:03:34 +0800

    important things

--- src/main/java/com/example/demogit/Test.java
+++ src/main/java/com/example/demogit/Test.java
@@ -9,5 +9,8 @@ public class Test {
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
     }
 }
